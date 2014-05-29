901f53d71330ec948d0101b2fa26d1cfa50afc99
hubharani <hubharani@ebay.com>
2014-05-28 18:20:55 -0700

    fixed the icon bug

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,19 +1,22 @@
 window.pp = window.pp || {};
 pp.convert = {
+    baseClasses: ' fa fa-2x form-control-feedback',
     init: function() {
+        var that = this;
         $('form').on('submit', this.onclick);
         $('select').on('focus', this.preserveOldVal);
         $('select').on('change', this.handleSelectChange.bind(this));
         $('select').each(function(index, element) {
             var el = $(element);
             var symbol = el.val().toLowerCase();
+            // element.prev = symbol;
             if (el.attr('name') === 'from') {
                 //to case
-                $('input[name="val"] ~ i').addClass('fa-' + symbol);
+                $('input[name="val"] ~ i').addClass('fa-' + symbol + that.baseClasses);
 
             } else {
                 //to
-                $('input.result ~ i').addClass('fa-' + symbol);
+                $('input.result ~ i').addClass('fa-' + symbol + that.baseClasses);
 
             }
             el.addClass('fa-' + symbol);
@@ -28,23 +31,23 @@ pp.convert = {
             $('.result').val(data.result);
         });
     },
-    preserveOldVal: function(e) {
-        e.target.prev = e.target.options[e.target.selectedIndex].text;
-    },
+    // preserveOldVal: function(e) {
+    //     e.target.prev = e.target.options[e.target.selectedIndex].text;
+    // },
     handleSelectChange: function(e) {
         var tar = $(e.target);
         var symbol = e.target.options[e.target.selectedIndex].text.toLowerCase();
         var cls = '';
-        if (e.target.prev != 'undefined') {
-            cls = 'fa-' + e.target.prev.toLowerCase();
-        }
+        // if (e.target.prev != 'undefined') {
+        //     cls = 'fa-' + e.target.prev.toLowerCase();
+        // }
         if (tar.attr('name') === 'from') {
             //from case
-            $('input[name="val"] ~ i').removeClass('fa-' + cls).addClass('fa-' + symbol);
+            $('input[name="val"] ~ i').removeClass().addClass('fa-' + symbol + this.baseClasses);
 
         } else {
             //to
-            $('input.result ~ i').removeClass('fa-' + cls).addClass('fa-' + symbol);
+            $('input.result ~ i').removeClass().addClass('fa-' + symbol + this.baseClasses);
 
         }
         this.preserveOldVal(e);
